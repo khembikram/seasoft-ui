17951b9335c6bcd860de950b629cce26390c41d1
Sujan Rana <khembikram1@gmail.com>
2023-03-23 00:43:14 -0700

    init commit

---
+++ splashsoft.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $(".icon").click(function () {
+    $("#myLinks").toggle();
+  });
+  $(".faq_view").click(function () {
+    $(viewcont).show();
+  });
+});
+
+
+// tabs 
+(function($, document) {
+    
+  // get tallest tab__content element
+  let height = -1;
+
+$('.tab__content').each(function() {
+  height = height > $(this).outerHeight() ? height : $(this).outerHeight();
+     $(this).css('position', 'absolute');
+});
+  
+  // set height of tabs + top offset
+$('[data-tabs]').css('min-height', height + 40 + 'px');
+
+}(jQuery, document));
